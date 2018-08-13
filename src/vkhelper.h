1fa2991f36bf42020e36a36d14c43de2cd0607c0
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2018-08-13 11:46:30 +0300

    [MERGE_FROM_ANDROID] Merge following commits:
    [bb6555] Energy saving: use less frequent timers where appropriate, stop RequestQueueTimer when there is no requests in queue and restart it when requests arrive.
    [26b278] Energy saving: try to send data immediately on change, use SendDataTimer only when we should wait SEND_DATA_INTERVAL seconds.

--- src/vkhelper.h
+++ src/vkhelper.h
@@ -56,7 +56,7 @@ public:
     static const int DEFAULT_MAX_TRUSTED_FRIENDS_COUNT    = 5,
                      DEFAULT_MAX_TRACKED_FRIENDS_COUNT    = 5,
                      REQUEST_QUEUE_TIMER_INTERVAL         = 1000,
-                     SEND_DATA_TIMER_INTERVAL             = 1000,
+                     SEND_DATA_TIMER_INTERVAL             = 60000,
                      SEND_DATA_INTERVAL                   = 300,
                      UPDATE_TRACKED_FRIENDS_DATA_INTERVAL = 60,
                      MAX_BATCH_SIZE                       = 25,
@@ -180,7 +180,6 @@ private:
     void ProcessGroupsJoinResponse(QString response, QVariantMap resp_request);
     void ProcessGroupsJoinError(QVariantMap err_request);
 
-    bool                         CurrentDataUpdated;
     int                          AuthState, MaxTrustedFriendsCount, MaxTrackedFriendsCount;
     qint64                       LastSendDataTime, LastUpdateTrackedFriendsDataTime;
     QString                      UserId, FirstName, LastName, PhotoUrl, BigPhotoUrl,
