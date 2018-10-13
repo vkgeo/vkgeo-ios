4b469e1f9dd1bed02f889675127c3e4f61bb2f5b
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2018-10-13 15:47:46 +0300

    Update scripts for Qt 5.11.2.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PATH=$PATH:~/Qt/5.9.1/ios/bin
+PATH=$PATH:~/Qt/5.11.2/ios/bin
 
 lupdate ../vkgeo.pro -ts ../translations/vkgeo_ru.src.ts
 lupdate ../qml       -ts ../translations/vkgeo_ru.qml.ts
