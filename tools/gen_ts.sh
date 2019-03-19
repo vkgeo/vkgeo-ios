5c887544ab5e10ec0df7a55b631f5337a817bf51
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-19 20:50:34 +0300

    [MERGE_FROM_ANDROID] Merge following commits:
    [aaaa6544] Simplify extra Qt localization for MessageDialog Yes/No buttons.

--- tools/gen_ts.sh
+++ tools/gen_ts.sh
@@ -5,4 +5,4 @@ PATH=$PATH:~/Qt/5.12.1/ios/bin
 lupdate ../vkgeo.pro -ts ../translations/vkgeo_ru.src.ts
 lupdate ../qml       -ts ../translations/vkgeo_ru.qml.ts
 
-lconvert ../translations/vkgeo_ru.src.ts ../translations/vkgeo_ru.qml.ts -o ../translations/vkgeo_ru.ts
+lconvert ../translations/vkgeo_ru.src.ts ../translations/vkgeo_ru.qml.ts ../translations/vkgeo_ru.qt.ts -o ../translations/vkgeo_ru.ts
