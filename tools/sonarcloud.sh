edf4294c3b33779da5b34d5bac07ef114814e7ce
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-04-21 00:54:23 +0300

    Added script for SonarCloud. It seems mostly useless for now, because Sonar scanner doesn't recognize .mm files as objective-c++ files.

---
+++ tools/sonarcloud.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PATH=$PATH:~/Qt/5.12.2/ios/bin:~/SonarCloud/build-wrapper-macosx-x86:~/SonarCloud/sonar-scanner-3.3.0.1492-macosx/bin
+
+if [ "$#" = "1" ]; then
+    cd .. && \
+    mkdir -p .sonarbuild && \
+    cd .sonarbuild && \
+    qmake ../vkgeo.pro && \
+    build-wrapper-macosx-x86 --out-dir bw-output make clean debug-device && \
+    cd .. && \
+    sonar-scanner -Dsonar.projectKey=vkgeo:vkgeo-ios \
+                  -Dsonar.projectName="VKGeo iOS" \
+                  -Dsonar.organization=vkgeo-github \
+                  -Dsonar.sources=. \
+                  -Dsonar.sourceEncoding=UTF-8 \
+                  -Dsonar.exclusions="qml/**/*,translations/*" \
+                  -Dsonar.cfamily.build-wrapper-output=.sonarbuild/bw-output \
+                  -Dsonar.host.url=https://sonarcloud.io \
+                  -Dsonar.login="$1"
+else
+    echo "Syntax: sonarcloud.sh SONARCLOUD_KEY"
+fi
