d5e08d0a47e761c330f59e8e00f7fd1c4ac392ee
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-04-23 14:17:46 +0300

    Style nits.

--- tools/sonarcloud.sh
+++ tools/sonarcloud.sh
@@ -15,8 +15,8 @@ if [ "$#" = "1" ]; then
                   -Dsonar.sources=. \
                   -Dsonar.sourceEncoding=UTF-8 \
                   -Dsonar.exclusions="qml/**/*,translations/*" \
-                  -Dsonar.cpp.file.suffixes=.cpp,.mm \
                   -Dsonar.cfamily.build-wrapper-output=.sonarbuild/bw-output \
+                  -Dsonar.cpp.file.suffixes=.cpp,.mm \
                   -Dsonar.host.url=https://sonarcloud.io \
                   -Dsonar.login="$1"
 else
