8bba0998ecc528aad57b45007749deaacdad3c8c
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-07-31 00:06:07 +0300

    Update translations.

--- translations/vkgeo_ru.qml.ts
+++ translations/vkgeo_ru.qml.ts
@@ -13,7 +13,7 @@
     </message>
     <message>
         <source>We keep this app free by showing ads. Ad network will &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;collect data and use a unique identifier on your device&lt;/a&gt; to show you ads. &lt;b&gt;Do you allow to use your data to tailor ads for you?&lt;/b&gt; You can change your choice anytime in the app settings.</source>
-        <translation type="unfinished">Это приложение распространяется бесплатно за счет показа в нем рекламы. Рекламная сеть будет &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;собирать данные и использовать уникальный идентификатор вашего устройства&lt;/a&gt; для того, чтобы показывать вам рекламные объявления. Разрешаете ли вы использовать свои данные для выбора наиболее подходящих для вас рекламных объявлений? Вы можете изменить свой выбор в любое время в настройках приложения.</translation>
+        <translation>Это приложение распространяется бесплатно за счет показа в нем рекламы. Рекламная сеть будет &lt;a href=&quot;https://policies.google.com/technologies/ads&quot;&gt;собирать данные и использовать уникальный идентификатор вашего устройства&lt;/a&gt; для того, чтобы показывать вам рекламные объявления. &lt;b&gt;Разрешаете ли вы использовать свои данные для выбора наиболее подходящих для вас рекламных объявлений?&lt;/b&gt; Вы можете изменить свой выбор в любое время в настройках приложения.</translation>
     </message>
 </context>
 <context>
