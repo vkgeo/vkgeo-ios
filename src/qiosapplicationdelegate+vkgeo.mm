c3b0da65d36a5399f064ee1558aa4342f8e07465
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-06-13 09:02:29 +0300

    Bump VK API version to 5.131.

--- src/qiosapplicationdelegate+vkgeo.mm
+++ src/qiosapplicationdelegate+vkgeo.mm
@@ -10,7 +10,7 @@
 namespace {
 
 const QString VK_APP_ID(QStringLiteral("6459902")),
-              VK_API_V (QStringLiteral("5.120"));
+              VK_API_V (QStringLiteral("5.131"));
 
 LocationManagerDelegate *LocationManagerDelegateInstance = nil;
 
